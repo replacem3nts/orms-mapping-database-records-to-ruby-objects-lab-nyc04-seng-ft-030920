4bf67131907273640970f500df3dcde99fc3948b
replacem3nts <donny.landis@gmail.com>
2020-03-22 22:20:19 -0400

    updates file to pass tests

--- lib/student.rb
+++ lib/student.rb
@@ -2,19 +2,75 @@ class Student
   attr_accessor :id, :name, :grade
 
   def self.new_from_db(row)
-    # create a new Student object given a row from the database
+    student = self.new
+    student.id = row[0]
+    student.name = row[1]
+    student.grade = row[2]
+    student
   end
 
   def self.all
-    # retrieve all the rows from the "Students" database
-    # remember each row should be a new instance of the Student class
+    sql = <<-SQL
+      SELECT *
+      FROM students;
+      SQL
+
+      DB[:conn].execute(sql).map {|row| self.new_from_db(row)}
   end
 
   def self.find_by_name(name)
-    # find the student in the database given a name
-    # return a new instance of the Student class
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE name = ?;
+      SQL
+
+      student = DB[:conn].execute(sql, name).map {|row| self.new_from_db(row)}
+      student.first
   end
-  
+
+  def self.all_students_in_grade_9
+    self.all_students_in_grade_X(9)
+  end
+ 
+  def self.students_below_12th_grade
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE grade < 12;
+      SQL
+
+      students = DB[:conn].execute(sql).map {|row| self.new_from_db(row)}
+      students
+  end
+
+  def self.first_X_students_in_grade_10(num)
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE grade = 10
+      LIMIT ?;
+      SQL
+
+      students = DB[:conn].execute(sql, num).map {|row| self.new_from_db(row)}
+      students
+  end
+
+  def self.first_student_in_grade_10
+    self.first_X_students_in_grade_10(1).first
+  end
+
+  def self.all_students_in_grade_X(grade)
+    sql = <<-SQL
+    SELECT *
+    FROM students
+    WHERE grade = ?;
+    SQL
+
+    students = DB[:conn].execute(sql, grade).map {|row| self.new_from_db(row)}
+    students
+  end
+
   def save
     sql = <<-SQL
       INSERT INTO students (name, grade) 
